dd1e093ad2bc0b6d1790b374f77cb05d21cd0e15
Boonie Attawut <puifai.ba@gmail.com>
2021-01-10 17:42:51 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -16,11 +16,17 @@ def consolidate_cart(cart)
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
   consolidated_array = []
+  cart 
   cart.each do |items|
     if find_item_by_name_in_collection(cart[:item], cart)
       consolidated_array[:count] += 1
     else
-      consolidated_array[:count] = 1
+      consolidated_array = [{
+        :item => cart[:item],
+        :price => cart[:price],
+        :clearance => cart[:clearance],
+        :count => 1
+        }]
     end
   end
   return consolidated_array
